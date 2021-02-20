997de591928361a7a35f90fec7aeb61847bd540a
Lenny Davis <49523080+ladavis4@users.noreply.github.com>
2021-02-20 11:29:53 -0500

    removed the guy who is 711

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 
-path = "C:/Users/ladav/PycharmProjects/DataChallenge/Data_Lv2_NCSG_BehaviorChangesCOVID19.csv"
+path = "C:/Users/ladav/ew430/datachallenge/Data_Lv2_NCSG_BehaviorChangesCOVID19.csv"
 
 df = pd.read_csv(path, encoding='cp1252')
 
-ages = df['Age'].astype(int)
-kids = df['Kids'].astype(int)
-plt.scatter(ages, kids)
+ages = df['Age']
+kids = df['Kids']
+plt.hist(ages, bins = 5)
 plt.show()
 
 
